980de23922a56c054c07ccb089cdbd0f9fd2f72a
digitalsleuth <corey@digitalsleuth.ca>
2026-06-16 01:29:13 +0000

    Linting and restructuring all around

--- naft/modules/ii.py
+++ naft/modules/ii.py
@@ -1,94 +1,99 @@
 #!/usr/bin/env python3
 
-__description__ = 'Network Appliance Forensic Toolkit - IOS Image'
-__version__ = '1.0.1'
-__original_author__ = 'Didier Stevens'
-__current_authors__ = '@digitalsleuth and @G-K7'
-__date__ = '2026/06/14'
+__description__ = "Network Appliance Forensic Toolkit - IOS Image"
+__version__ = "1.0.1"
+__original_author__ = "Didier Stevens"
+__current_authors__ = "@digitalsleuth and @G-K7"
+__date__ = "2026/06/15"
 
 import hashlib
-import glob
-import os
 import math
 import pickle
 import traceback
 import time
-import re
 from pathlib import Path
 import naft.modules.uf as uf
 import naft.modules.iipf as iipf
-import naft.modules.impf as impf
+
+
+oMD5Database = None
 
 
 def CiscoIOSImageFileParser(filename, arguments):
     global oMD5Database
     image = uf.File2Data(filename)
     if image is None:
-        print('Error reading {}'.format(filename))
+        print(f"Error reading {filename}")
         return
     oIOSImage = uf.InProgress(iipf.cIOSImage, image, filename)
     oIOSImage.Print()
-    if arguments['md5db']:
-        if arguments['md5db'] is not None:
-            oMD5Database = iipf.cMD5Database(arguments['md5db'], arguments['scan'])
+    if arguments["md5db"]:
+        if arguments["md5db"] is not None:
+            oMD5Database = iipf.cMD5Database(arguments["md5db"], arguments["scan"])
         md5hash = hashlib.md5(image).hexdigest()
         filenameCSV, filenameDB, filedateDB = oMD5Database.Find(md5hash)
         if filenameCSV is None:
-            print('File not found in md5 database')
+            print("File not found in md5 database")
         else:
-            print('File found in md5 database: {}, filename: {}, dated: {}'.format(filenameCSV, filenameDB, filedateDB))
-    if arguments['verbose']:
-        print('\nELF Headers:\n')
-        print(f"index {'index_str': >10} {'type': >10} {'flags': >10} {'offset': >10} {'size': >10} {'data': >10}")
-        for oSectionHeader in oIOSImage.oELF.sections:
-            print('   {:2d}    {:>7s} {:>10d} {:10d}   {:08X} {:>10d}       {}'.format(
-                oSectionHeader.nameIndex,
-                oSectionHeader.nameIndexString,
-                oSectionHeader.type,
-                oSectionHeader.flags,
-                oSectionHeader.offset,
-                oSectionHeader.size,
-                repr(oSectionHeader.sectionData[0:8]))
+            print(
+                f"File found in md5 database: {filenameCSV}, filename: {filenameDB}, dated: {filedateDB}"
             )
-    if arguments['extract']:
-        print("\n{} written to: {}".format(oIOSImage.imageUncompressedName, arguments['extract']))
-        uf.Data2File(oIOSImage.imageUncompressed, oIOSImage.imageUncompressedName, arguments['extract'])
-    if arguments['ida']:
+    if arguments["verbose"]:
+        print("\nELF Headers:\n")
+        print(
+            f"index {'index_str': >10} {'type': >10} {'flags': >10} {'offset': >10} {'size': >10} {'data': >10}"
+        )
+        for oSectionHeader in oIOSImage.oELF.sections:
+            print(
+                f"   {oSectionHeader.nameIndex:2d}    {oSectionHeader.nameIndexString:>7s} {oSectionHeader.type:>10d} {oSectionHeader.flags:10d}   {oSectionHeader.offset:08X} {oSectionHeader.size:>10d}       {repr(oSectionHeader.sectionData[0:8])}"
+            )
+    if arguments["extract"]:
+        print(f"\n{oIOSImage.imageUncompressedName} written to: {arguments['extract']}")
+        uf.Data2File(
+            oIOSImage.imageUncompressed,
+            oIOSImage.imageUncompressedName,
+            arguments["extract"],
+        )
+    if arguments["ida"]:
         print("\nPatching for IDA Pro...")
         time.sleep(0.5)
-        print("{} written to: {}".format(oIOSImage.imageUncompressedName, arguments['ida']))
-        uf.Data2File(oIOSImage.ImageUncompressedIDAPro(), oIOSImage.imageUncompressedName, arguments['ida'])
+        print(f"{oIOSImage.imageUncompressedName} written to: {arguments['ida']}")
+        uf.Data2File(
+            oIOSImage.ImageUncompressedIDAPro(),
+            oIOSImage.imageUncompressedName,
+            arguments["ida"],
+        )
 
 
 def Entropy(data):
     result = 0.0
     size = len(data)
     if size != 0:
-        bucket = [0]*256
+        bucket = [0] * 256
         for char in data:
             bucket[char] += 1
         for count in bucket:
             if count > 0:
                 percentage = float(count) / size
-                result -= percentage*math.log(percentage, 2)
+                result -= percentage * math.log(percentage, 2)
     return result
 
 
-def TargetDir(dir, arguments):
+def TargetDir(directory, arguments):
     bins = []
-    tdir = Path(dir)
+    tdir = Path(directory)
     R = False
     if tdir.is_dir():
-        if arguments['recurse']:
+        if arguments["recurse"]:
             R = True
-            for child in tdir.rglob('*'):
-                if child.is_file() and child.suffix == '.bin':
+            for child in tdir.rglob("*"):
+                if child.is_file() and child.suffix == ".bin":
                     bins.append(child)
         else:
             for child in tdir.iterdir():
-                if child.is_file() and child.suffix == '.bin':
+                if child.is_file() and child.suffix == ".bin":
                     bins.append(child)
-    if tdir.is_file() and tdir.suffix == '.bin':
+    if tdir.is_file() and tdir.suffix == ".bin":
         bins.append(tdir)
     return tdir, bins, R
 
@@ -96,116 +101,143 @@ def TargetDir(dir, arguments):
 def vn(dictionary, key):
     if key in dictionary:
         return dictionary[key]
-    else:
-        return None
+    return None
 
 
 def PickleData(data):
-    fPickle = open('resume.pkl', 'wb')
-    pickle.dump(data, fPickle)
-    fPickle.close()
-    print('Pickle file saved')
+    with open("resume.pkl", "wb") as fPickle:
+        pickle.dump(data, fPickle)
+    print("Pickle file saved")
 
 
-def CiscoIOSImageFileScanner(dir, arguments):
-    if not arguments['resume']:
-        tdir, filenames, R = TargetDir(dir, arguments)
+def CiscoIOSImageFileScanner(directory, arguments):
+    global oMD5Database
+    if not arguments["resume"]:
+        tdir, filenames, R = TargetDir(directory, arguments)
         if not filenames:
-            print('No image(s) found. Verify FILE/DIR path.')
+            print("No image(s) found. Verify FILE/DIR path.")
             return
-        print('Target path: {}'.format(tdir.resolve()))
-        if arguments['recurse'] and R == True:
-            print('Recursive search')
+        print(f"Target path: {tdir.resolve()}")
+        if arguments["recurse"] and R is True:
+            print("Recursive search")
         countFilenames = len(filenames)
-        print("Performing scan on {:d} file(s):\n".format(countFilenames))
+        print(f"Performing scan on {countFilenames:d} file(s):\n")
         counter = 1
-        if arguments['log'] is not None:
-            f = open(arguments['log'], 'w')
-            f.close()
     else:
-        fPickle = open(arguments['resume'], 'rb')
-        filenames, countFilenames, counter = pickle.load(fPickle)
-        fPickle.close()
-        print('Pickle file loaded')
+        with open(arguments["resume"], "rb") as fPickle:
+            filenames, countFilenames, counter = pickle.load(fPickle)
+        print("Pickle file loaded")
     scan_header = [
-        '#',
-        'filename',
-        'CW_VERSION',
-        'CW_FAMILY',
-        'imageSize',
-        'entropy',
-        'errorCode',
-        'oELFerrorCode',
-        'oELFsectionCount',
-        'oELFstringTableIndex',
-        'cksumCompressed',
-        'cksumCompEqCalculated',
-        'cksumUncompressed',
-        'cksumUncompEqCalculated',
-        'uncompressedFilename',
-        'embeddedMD5'
-        ]
-    if arguments['md5db']:
-        scan_header.extend(['md5hash', 'csvFilename', 'dbFilename', 'dbFileDate'])
-    print(','.join(scan_header))
-    while len(filenames) > 0:
-        filename = filenames[0]
-        try:
-            line = [str(counter), str(filename.name)]
-            image = uf.File2Data(filename)
-            if image is None:
-                line.extend(['Error reading'])
-            else:
-                oIOSImage = uf.InProgress(iipf.cIOSImage, image, filename.name)
-                if oIOSImage.oCWStrings is not None and oIOSImage.oCWStrings.error is None:
-                    line.extend([(uf.cn(vn(oIOSImage.oCWStrings.dCWStrings, b'CW_VERSION'))).decode(),
-                                 (uf.cn(vn(oIOSImage.oCWStrings.dCWStrings, b'CW_FAMILY'))).decode()])
+        "#",
+        "filename",
+        "CW_VERSION",
+        "CW_FAMILY",
+        "imageSize",
+        "entropy",
+        "errorCode",
+        "oELFerrorCode",
+        "oELFsectionCount",
+        "oELFstringTableIndex",
+        "cksumCompressed",
+        "cksumCompEqCalculated",
+        "cksumUncompressed",
+        "cksumUncompEqCalculated",
+        "uncompressedFilename",
+        "embeddedMD5",
+    ]
+    if arguments["md5db"]:
+        scan_header.extend(["md5hash", "csvFilename", "dbFilename", "dbFileDate"])
+    print(",".join(scan_header))
+    if arguments["md5db"]:
+        oMD5Database = iipf.cMD5Database(arguments["md5db"], arguments["scan"])
+
+    interrupted = False
+
+    def run_scan(log_file=None):
+        nonlocal counter, filenames, interrupted
+        while filenames:
+            filename = filenames[0]
+            try:
+                line = [str(counter), str(filename.name)]
+                image = uf.File2Data(filename)
+                if image is None:
+                    line.extend(["Error reading"])
                 else:
-                    line.extend([uf.cn(None), uf.cn(None)])
-                if oIOSImage.checksumCompressed is None:
-                    checksumCompressed = 'Not found'
-                else:
-                    checksumCompressed = '0x{:08X}'.format(oIOSImage.checksumCompressed)
-                if oIOSImage.checksumUncompressed is None:
-                    checksumUncompressed = 'Not found'
-                else:
-                    checksumUncompressed = '0x{:08X}'.format(oIOSImage.checksumUncompressed)
-                line.extend([
-                    str(len(image)),
-                    '{:.2f}'.format(Entropy(image)),
-                    str(oIOSImage.error),
-                    str(oIOSImage.oELF.error),
-                    str(oIOSImage.oELF.countSections),
-                    str(uf.cn(oIOSImage.oELF.stringTableIndex)),
-                    str(uf.cn(oIOSImage.checksumCompressed, '0x{:08X}')),
-                    str(oIOSImage.checksumCompressed is not None and
-                        oIOSImage.checksumCompressed == oIOSImage.calculatedChecksumCompressed),
-                    str(uf.cn(oIOSImage.checksumUncompressed, '0x{:08X}')),
-                    str(oIOSImage.checksumUncompressed is not None and
-                        oIOSImage.checksumUncompressed == oIOSImage.calculatedChecksumUncompressed),
-                    uf.cn(oIOSImage.imageUncompressedName),
-                    uf.cn(oIOSImage.embeddedMD5)
-                ])
-                if arguments['md5db']:
-                    oMD5Database = iipf.cMD5Database(arguments['md5db'], arguments['scan'])
-                    md5hash = hashlib.md5(image).hexdigest()
-                    filenameCSV, filenameDB, filedateDB = oMD5Database.Find(md5hash)
-                    line.extend([md5hash, uf.cn(filenameCSV), uf.cn(filenameDB), uf.cn(filedateDB)])
-            strLine = ','.join(line)
-            print(strLine)
-            if arguments['log'] is not None:
-                f = open(arguments['log'], 'a')
-                f.write(strLine + '\n')
-                f.close()
-            counter += 1
-            filenames = filenames[1:]
-        except KeyboardInterrupt:
-            print('KeyboardInterrupt')
-            PickleData([filenames, countFilenames, counter])
-            return
-        except:
-            traceback.print_exc()
-            PickleData([filenames, countFilenames, counter])
-            return
-    print('')
-    print('{:d} file(s) scanned.'.format(counter-1))
+                    oIOSImage = uf.InProgress(iipf.cIOSImage, image, filename.name)
+                    if (
+                        oIOSImage.oCWStrings is not None
+                        and oIOSImage.oCWStrings.err is None
+                    ):
+                        line.extend(
+                            [
+                                uf.cn(
+                                    vn(oIOSImage.oCWStrings.dCWStrings, b"CW_VERSION")
+                                ).decode(),
+                                uf.cn(
+                                    vn(oIOSImage.oCWStrings.dCWStrings, b"CW_FAMILY")
+                                ).decode(),
+                            ]
+                        )
+                    else:
+                        line.extend([uf.cn(None), uf.cn(None)])
+                    line.extend(
+                        [
+                            str(len(image)),
+                            f"{Entropy(image):.2f}",
+                            str(oIOSImage.err),
+                            str(oIOSImage.oELF.err),
+                            str(oIOSImage.oELF.countSections),
+                            str(uf.cn(oIOSImage.oELF.stringTableIndex)),
+                            str(uf.cn(oIOSImage.checksumCompressed, "0x{:08X}")),
+                            str(
+                                oIOSImage.checksumCompressed is not None
+                                and oIOSImage.checksumCompressed
+                                == oIOSImage.calculatedChecksumCompressed
+                            ),
+                            str(uf.cn(oIOSImage.checksumUncompressed, "0x{:08X}")),
+                            str(
+                                oIOSImage.checksumUncompressed is not None
+                                and oIOSImage.checksumUncompressed
+                                == oIOSImage.calculatedChecksumUncompressed
+                            ),
+                            uf.cn(oIOSImage.imageUncompressedName),
+                            uf.cn(oIOSImage.embeddedMD5),
+                        ]
+                    )
+                    if arguments["md5db"]:
+                        md5hash = hashlib.md5(image).hexdigest()
+                        filenameCSV, filenameDB, filedateDB = oMD5Database.Find(md5hash)
+                        line.extend(
+                            [
+                                md5hash,
+                                uf.cn(filenameCSV),
+                                uf.cn(filenameDB),
+                                uf.cn(filedateDB),
+                            ]
+                        )
+                strLine = ",".join(line)
+                print(strLine)
+                if log_file is not None:
+                    log_file.write(strLine + "\n")
+                counter += 1
+                filenames = filenames[1:]
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt")
+                PickleData([filenames, countFilenames, counter])
+                interrupted = True
+                return
+            except:
+                traceback.print_exc()
+                PickleData([filenames, countFilenames, counter])
+                interrupted = True
+                return
+
+    if arguments["log"] is not None:
+        log_mode = "a" if arguments["resume"] else "w"
+        with open(arguments["log"], log_mode, encoding="utf-8") as log_file:
+            run_scan(log_file)
+    else:
+        run_scan()
+    if not interrupted:
+        print("")
+        print(f"{(counter - 1):d} file(s) scanned.")
--- naft/modules/uf.py
+++ naft/modules/uf.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-__description__ = 'Network Appliance Forensic Toolkit - Utility Functions'
-__version__ = '1.0.1'
-__original_author__ = 'Didier Stevens'
-__current_authors__ = '@digitalsleuth and @G-K7'
-__date__ = '2026/06/14'
+__description__ = "Network Appliance Forensic Toolkit - Utility Functions"
+__version__ = "1.0.1"
+__original_author__ = "Didier Stevens"
+__current_authors__ = "@digitalsleuth and @G-K7"
+__date__ = "2026/06/15"
 
 import time
 import os
@@ -13,7 +13,7 @@ import sys
 from concurrent.futures import ProcessPoolExecutor
 from dateutil import parser as duparser
 
-MALWARE_PASSWORD = 'infected'
+MALWARE_PASSWORD = "infected"
 
 
 def InProgress(function, *args):
@@ -22,7 +22,11 @@ def InProgress(function, *args):
     pool = ProcessPoolExecutor(3)
     future = pool.submit(function, *args)
     while not future.done():
-        print("Processing... {}".format(animation[idx % len(animation)]), end="\r", file=sys.stderr)
+        print(
+            f"Processing... {animation[idx % len(animation)]}",
+            end="\r",
+            file=sys.stderr,
+        )
         idx += 1
         time.sleep(0.1)
     return future.result()
@@ -30,49 +34,34 @@ def InProgress(function, *args):
 
 def IsZIPFile(filename):
     filename = os.path.basename(filename)
-    return filename.lower().endswith('.zip')
+    return filename.lower().endswith(".zip")
 
 
 def File2Data(filename):
-    if IsZIPFile(filename):
-        oZipfile = zipfile.ZipFile(filename, 'r')
-        oZipContent = oZipfile.open(oZipfile.infolist()[0], 'r', MALWARE_PASSWORD)
-        try:
-            return oZipContent.read()
-        except MemoryError:
-            return MemoryError
-        except:
-            return None
-        finally:
-            oZipContent.close()
-            oZipfile.close()
-
     try:
-        f = open(filename, 'rb')
-    except:
-        return None
-    try:
-        return f.read()
+        if IsZIPFile(filename):
+            with zipfile.ZipFile(filename, "r") as oZipfile:
+                with oZipfile.open(
+                    oZipfile.infolist()[0], "r", MALWARE_PASSWORD
+                ) as oZipContent:
+                    return oZipContent.read()
+        else:
+            with open(filename, "rb") as f:
+                return f.read()
     except MemoryError:
         return MemoryError
     except:
         return None
-    finally:
-        f.close()
 
 
 def Data2File(data, filename, path):
     try:
         full_path = os.path.join(path, filename)
-        f = open(full_path, 'wb')
+        f = open(full_path, "wb")
     except:
         return False
-    try:
+    with f:
         f.write(data)
-    except:
-        return False
-    finally:
-        f.close()
     return True
 
 
@@ -85,7 +74,7 @@ def SearchASCIIStrings(data, MIN_LENGTH=5):
             if iStringStart == -1:
                 iStringStart = iterant
             elif iterant + 1 == size and iterant - iStringStart + 1 >= MIN_LENGTH:
-                dStrings[iterant] = data[iStringStart:iterant + 1]
+                dStrings[iterant] = data[iStringStart : iterant + 1]
         elif iStringStart != -1:
             if iterant - iStringStart >= MIN_LENGTH:
                 dStrings[iterant] = data[iStringStart:iterant]
@@ -94,21 +83,25 @@ def SearchASCIIStrings(data, MIN_LENGTH=5):
 
 
 def DumpBytes(memory, baseAddress, WIDTH=16):
-    lineHex = ''
-    lineASCII = ''
+    lineHex = ""
+    lineASCII = ""
     for iterant in range(len(memory)):
-        lineHex += '{:02X} '.format(memory[iterant])
-        if chr(memory[iterant]) >= '\x20' and chr(memory[iterant]) <= '\x7F':
+        lineHex += f"{memory[iterant]:02X} "
+        if chr(memory[iterant]) >= "\x20" and chr(memory[iterant]) <= "\x7f":
             lineASCII += chr(memory[iterant])
         else:
-            lineASCII += '.'
+            lineASCII += "."
         if iterant % WIDTH == WIDTH - 1:
-            print(' {:08X}: {} {}'.format(int(baseAddress + iterant / WIDTH * WIDTH), lineHex, lineASCII))
-            lineHex = ''
-            lineASCII = ''
-    if lineHex != '':
-        lineHex += ' ' * (48 - len(lineHex))
-        print(' {:08X}: {} {}'.format(int(baseAddress + iterant / WIDTH * WIDTH), lineHex, lineASCII))
+            print(
+                f" {int(baseAddress + iterant / WIDTH * WIDTH):08X}: {lineHex} {lineASCII}"
+            )
+            lineHex = ""
+            lineASCII = ""
+    if lineHex != "":
+        lineHex += " " * (48 - len(lineHex))
+        print(
+            f" {int(baseAddress + iterant / WIDTH * WIDTH):08X}: {lineHex} {lineASCII}"
+        )
 
 
 def FindAllStrings(string, search):
@@ -120,9 +113,9 @@ def FindAllStrings(string, search):
     return indices
 
 
-def cn(value, output_format = None):
+def cn(value, output_format=None):
     if value is None:
-        return 'Not found'
+        return "Not found"
     if output_format is None:
         return value
     return output_format.format(value)
@@ -133,30 +126,30 @@ def Timestamp(epoch=None):
         localTime = time.localtime()
     else:
         localTime = time.localtime(epoch)
-    return time.strftime('%Y%m%d-%H%M%S', localTime)
+    return time.strftime("%Y%m%d-%H%M%S", localTime)
 
 
 def LogLine(line):
-    print('{}: {}'.format(Timestamp(), line))
+    print(f"{Timestamp()}: {line}")
 
 
 def ParseDateTime(dtg_str, time_format=None):
     if dtg_str is None:
-        return 'No date found'
+        return "No date found"
     parsed_date = duparser.parse(dtg_str)
     if time_format is None:
         return parsed_date
     return parsed_date.strftime(time_format)
 
 
-class cBufferFile():
+class cBufferFile:
 
     def __init__(self, filename, buffersize, bufferoverlapsize):
         self.filename = filename
         self.buffersize = buffersize
         self.bufferoverlapsize = bufferoverlapsize
         self.fIn = None
-        self.error = False
+        self.err = False
         self.index = None
         self.buffer = None
         self.filesize = os.path.getsize(self.filename)
@@ -165,44 +158,39 @@ class cBufferFile():
     def Read(self):
         if self.fIn is None:
             try:
-                self.fIn = open(self.filename, 'rb')
+                self.fIn = open(self.filename, "rb")
             except:
-                self.error = True
+                self.err = True
                 return False
-        if self.index is None:
-            self.index = 0
-            try:
-                self.buffer = self.fIn.read(self.buffersize + self.bufferoverlapsize)
-                self.bytesread += len(self.buffer)
-            except MemoryError:
-                self.fIn.close()
-                self.error = MemoryError
-                return False
-            except:
-                self.fIn.close()
-                self.error = True
-                return False
-            if self.buffer == '':
-                self.fIn.close()
-                return False
-            return True
-        self.buffer = self.buffer[-self.bufferoverlapsize:]
+
         try:
-            tempBuffer = self.fIn.read(self.buffersize)
-            if tempBuffer == '':
+            if self.index is None:
+                self.index = 0
+                self.buffer = self.fIn.read(self.buffersize + self.bufferoverlapsize)
+            else:
+                self.buffer = self.buffer[-self.bufferoverlapsize :]
+                tempBuffer = self.fIn.read(self.buffersize)
+                if not tempBuffer:
+                    self.fIn.close()
+                    return False
+                self.buffer += tempBuffer
+                self.index += self.buffersize
+                self.bytesread += len(tempBuffer)
+                return True
+
+            if not self.buffer:
                 self.fIn.close()
                 return False
-            self.buffer += tempBuffer
-            self.index += self.buffersize
-            self.bytesread += len(tempBuffer)
+            self.bytesread += len(self.buffer)
             return True
+
         except MemoryError:
             self.fIn.close()
-            self.error = MemoryError
+            self.err = MemoryError
             return False
         except:
             self.fIn.close()
-            self.error = True
+            self.err = True
             return False
 
     def Progress(self):
